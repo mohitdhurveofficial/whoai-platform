025a291515e69080452f609fac519e9e59c2e98a
Mohit Dhurve <mhtdv420@gmail.com>
2026-05-29 18:39:46 +0530

    Fix approvals page export

---
+++ app/approvals/page.tsx
@@ -0,0 +1,17 @@
+export default function ApprovalsPage() {
+
+  return (
+
+    <main className="p-8">
+
+      <h1 className="text-3xl font-bold">
+
+        Approvals
+
+      </h1>
+
+    </main>
+
+  );
+
+}
